4641d5b8ed62a7bd8ec3d810ea6ebbd0f210c85b
nozila2024 <nozilarajabboyeva@gmail.com>
2025-11-12 18:09:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,30 @@
+#1-misol
+n = input("Ism kiriting: ")
+print(n.upper())
+
+#2-misol
+n = "PyThonDaSTuRlAsh"
+print(n.lower())
+
+#3-misol
+n = input("Parol kiriting: ")
+print(n.lower())
+if n == "admin":
+    print("Xavfsiz parol emas")
+
+#4-misol
+n = "Salom dunyo  "
+print(n.strip())
+
+#5-misol
+n = input("email kiriting: ")
+print(n.strip())
+
+#6-misol
+n = input("Matn kiriting: ")
+print(n.lstrip())
+
+7-misol
+#6-misol
+n = input("Matn kiriting: ")
+print(n.rstrip())
